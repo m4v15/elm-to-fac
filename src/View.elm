9af74abbaf4af46585d0d056db4518974e32c3ca
Matt Davis <matthewpeterdavis@gmail.com>
2017-11-10 17:51:31 +0000

    modularise views, add tachyons

--- src/View.elm
+++ src/View.elm
@@ -6,25 +6,53 @@ module View exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+import Json.Decode as Json
 import Models exposing (..)
 
 
 view : Model -> Html Msg
 view model =
-    div [ class "w-60-ns center" ]
-        [ h1 [ class "tc f1" ] [ text "hello" ]
-        , p [ class "f3 w60 mh1 tc" ] [ text "Welcome to elm Travel to FAC London planner - enter your (London) postcode below and find out how long it would take to get to FAC. Go nuts." ]
-        , input [ class "f3 w30 pa1 center db ba", onInput Change, value model.postCode ] []
-        , button [ class "f3 w30 pa1 center db ba", onClick GetTime ] [ text "Submit" ]
-        , h1 [] [ text ("post code: " ++ model.postCode) ]
+    div [ class "w-80-ns center" ]
+        [ headerCustom
+        , postCodeInput model
         , div []
-            [ h1 []
-                [ text "Journeys: " ]
-            , div []
+            [ ul [ class "bg-black-70 list pl0 ml0 center mw7 ba b--light-silver br3" ]
                 (model.journeys
                     |> List.sortBy .duration
                     |> List.map eachJourney
                 )
+            ]
+        ]
+
+
+headerCustom : Html Msg
+headerCustom =
+    header [ class "tc pv2 pv5-ns" ]
+        [ img [ src "https://cdn4.iconfinder.com/data/icons/auto-icons-6/100/3-512.png", class "br-100 pa1 ba b--black-10 h3 w3" ] []
+        , h1
+            [ class "f5 f4-ns fw6 mid-gray" ]
+            [ text "How To Get to FAC" ]
+        , h2
+            [ class "f6 gray fw2 ttu tracked" ]
+            [ text "Welcome to elm Travel to FAC London planner - enter your (London) postcode below and find out how long it would take to get to FAC. Go nuts." ]
+        ]
+
+
+postCodeInput : Model -> Html Msg
+postCodeInput model =
+    div [ class "pa4-1 border-box" ]
+        [ Html.form
+            [ class
+                "bg-light-red mw7 center pa4 br2-ns ba b--black-10"
+            , onWithOptions "submit" { stopPropagation = False, preventDefault = True } (Json.succeed GetTime)
+            ]
+            [ fieldset [ class "ttu cf bn ma0 pa0" ]
+                [ div [ class "cf tc" ]
+                    [ label [ class "clip", for "postcode" ] [ text "Postcode" ]
+                    , input [ class "ttu f6 tc input-reset bn pa3 black-80 bg-white lh-solid w-90", placeholder "Postcode", onInput Change ] []
+                    , input [ class "ttu f6 button-reset tc bn pa3 bg-animate bg-black-70 hover-bg-black white pointer w-100", type_ "submit", value "Get routes!" ] []
+                    ]
+                ]
             ]
         ]
 
@@ -36,14 +64,14 @@ subscriptions model =
 
 eachJourney : Journey -> Html Msg
 eachJourney journey =
-    div [ class "f2" ]
-        [ text ("Time to FAC: " ++ toString journey.duration ++ " minutes")
-        , div
-            []
+    li [ class "white ph3 f2 pv2 bb b--light-silver ttu tc" ]
+        [ text (toString journey.duration ++ " minutes")
+        , ul
+            [ class "hover-bg-black bg-light-red list pl0 ml0 center mw7 ba b--light-silver br3" ]
             (List.map eachLeg (List.indexedMap (,) journey.legs))
         ]
 
 
 eachLeg : ( Int, Leg ) -> Html Msg
 eachLeg leg =
-    div [ class "f3 pa3 tc" ] [ div [ class "w-50 dib tl" ] [ text (toString (Tuple.first leg) ++ ". " ++ Tuple.second leg) ] ]
+    li [ class "ph3 f5 pv2 bb b--light-silver ttu tc center" ] [ div [ class "tc w-50 dib tl" ] [ text (Tuple.second leg) ] ]
